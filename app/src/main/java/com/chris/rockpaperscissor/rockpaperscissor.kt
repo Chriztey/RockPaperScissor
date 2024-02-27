2cd14f1033dd0493c68e4cc46dc666c83951a0cc
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-02-27 14:25:11 +0700

    Update rockpaperscissor.kt
    finish up to quiz 7

--- app/src/main/java/com/chris/rockpaperscissor/rockpaperscissor.kt
+++ app/src/main/java/com/chris/rockpaperscissor/rockpaperscissor.kt
@@ -4,20 +4,48 @@ fun main () {
     var computerChoice = ""
     var playerChoice = ""
 
-    println("Rock/Paper/Scissor. Please enter your choice!")
+    while (playerChoice !in listOf("rock", "paper", "scissor")) {
+        print("Enter your choice (rock, paper, or scissor): ")
+        playerChoice = readLine().toString().lowercase()
 
-    playerChoice = readln()
+        if (playerChoice !in listOf("rock", "paper", "scissor")) {
+            println("Invalid choice. Please enter a valid choice.")
+        }
+    }
+
 
     val randomNumber = (1..3).random()
 
     computerChoice = if (randomNumber == 1) {
-        "Rock"
+        "rock"
     }
     else if (randomNumber == 2) {
-        "Paper"
+        "paper"
     }
-    else "Scissor"
+    else "scissor"
 
     println(computerChoice)
+
+
+
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "rock" && computerChoice == "scissor" -> "Player"
+        playerChoice == "paper" && computerChoice == "rock" -> "Player"
+        playerChoice == "rock" && computerChoice == "scissor" -> "Player"
+        else -> {
+            "Computer"
+        }
+    }
+    if (winner == "Tie") {
+        println("It's a Tie !")
+    }
+
+    else println("$winner Win !")
+
+
+
+
+
 
 }
